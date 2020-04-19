28129a0a38d02f32e178eed6f2378e46c1dd731f
hornadam8 <hornadam8@gmail.com>
2020-04-19 13:38:23 -0600

    added trips table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200419190939) do
+ActiveRecord::Schema.define(version: 20200419193315) do
+
+  create_table "trips", force: :cascade do |t|
+    t.string  "review"
+    t.date    "visit_date"
+    t.integer "user_id"
+    t.integer "park_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
