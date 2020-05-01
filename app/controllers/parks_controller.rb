f4b0919b8f3db00ed7fc59e5455da8731eb1c012
hornadam8 <hornadam8@gmail.com>
2020-05-01 14:00:42 -0600

    added search bar to index

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -19,9 +19,13 @@ class ParksController < ApplicationController
     end
   end
 
-  private
-    def parks_params
-      params.require(:parks).permit(:name, :name, :search)
+  post "/parks/search" do
+    if Park.all.select{|park| park.name.include?(params[:search].titleize)}
+      @park = Park.all.select{|park| park.name.include?(params[:search].titleize)}[0]
+      redirect "/parks/#{@park.id}"
+    else
+      status 404
     end
+  end
 
 end
