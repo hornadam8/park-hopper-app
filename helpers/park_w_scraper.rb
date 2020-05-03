f6aca279a5877f733f58e701898975e73d052e54
hornadam8 <hornadam8@gmail.com>
2020-05-03 10:13:55 -0600

    cleaning up code

--- helpers/park_w_scraper.rb
+++ helpers/park_w_scraper.rb
@@ -23,7 +23,6 @@ class ParkScraper
 
   def assign_attributes
 
-    #Finds info
     page = Scraper.get_park_page(self.link).css(".fieldset-wrapper")
 
     if page.children[5].children[1]
@@ -32,7 +31,6 @@ class ParkScraper
      self.image = Scraper.get_park_page(self.link).css(".page").children[3].children[1].children[1].children[3].children[1].children[1].children[1].children[1].children[3].children[1].children[1].children[1].children[1].children[1].children[1].children[0].children[0].children[0].children[0].children[1].attributes["src"].to_s
     end
 
-    #Location(insert eye-roll emoji here)---------------------------------------
 
     location = page[3]
 
@@ -44,48 +42,39 @@ class ParkScraper
 
 
         if line_2
-          #Group of parks?(#2 Arches confirmed) with an extra piece of info(Box#)
           line_2_sub = line_2.children[0].text.gsub("\n","").chomp(" ")
           line_3 = location.children[1].children[3].children[3].children[5].children.text.gsub("\n","").chomp(" ")
           line_4 = location.children[1].children[3].children[3].children[7].children[0].text
           self.location = "#{line_1} #{line_2_sub},  #{line_3}, #{line_4}"
 
         else
-          #Most Parks
           alt_line_2 = location.children[1].children[3].children[3].children[5].children.text.gsub("\n","").chomp(" ")
           alt_line_3 = location.children[1].children[3].children[3].children[7].text
           self.location = "#{line_1},  #{alt_line_2}, #{alt_line_3}"
         end
 
       elsif location.children[1].children[3].children[3].children.to_a.length > 5
-        #49.RMNP (Page built differently, children is a single array
-        #as opposed to arrays separated by commas)
         line_1 = location.children[1].children[3].children[3].children[1].children[1].children.text
         line_2 = location.children[1].children[3].children[3].children[3].children.text.gsub("\n","").chomp(" ")
         line_3 = location.children[1].children[3].children[3].children[5].children.text
         self.location = "#{line_1},  #{line_2}, #{line_3}"
 
       else
-        #56.WSNP (Built differently because location is an AF base and
-        #doesnt have a street address)
         line_1 = location.children[1].children[3].children[3].children[1].children.text.gsub("\n","").chomp(" ")
         line_2 = location.children[1].children[3].children[3].children[3].children[0].text
         self.location = "#{line_1}, #{line_2}"
       end
 
     else
-      #Group of parks?(#6. BCGNP confirmed) children is a single array
       line_1 = location.children[0].children[3].children[3].children[3].children[1].text
       line_2 = location.children[0].children[3].children[3].children[5].children.text.gsub("\n","").chomp(" ")
       line_3 = location.children[0].children[3].children[3].children[7].children[0].text
       self.location = "#{line_1},  #{line_2}, #{line_3}"
     end
 
-    #Description--------------------------------------------------
 
     self.description = page[0].children[3].children.text.chomp("\n")
 
-    #Information--------------------------------------------------
 
     information = page[0].children[11]
 
@@ -95,7 +84,6 @@ class ParkScraper
       self.information = page[0].text.split("   ")[0].split("Information")[1].split("<")[0].strip
     end
 
-    #Weather-------------------------------------------------------
 
     weather = page[3].children[0].children[0].children[2]
 
@@ -105,7 +93,7 @@ class ParkScraper
       self.weather = "There is no information available on the weather for this park."
     end
 
-    #Tours and Camping---------------------------------------------
+
 
     tours_and_camping = page[3].children[0].children[1].children[2]
 
@@ -115,27 +103,21 @@ class ParkScraper
       self.tours_and_camping = "There is no information available on the tours and camping in this park."
     end
 
-    #Wildlife---------------------------------------------------------
+
 
     wildlife = page[3].children[0]
 
     if wildlife.children.to_a.length >= 3
-      #Most Parks
       if wildlife.children[2].children.to_a.length >= 3
         self.wildlife = wildlife.children[2].children[2].children[1].children.text
       else
-        #pages that have no wildlife section (and they're missing other info too i.e.
-        #6 BCGNP, which has almost none of it's info on the page)
         self.wildlife = "There is no information available on wildlife in this park."
       end
 
     else
-      #specifically Joshua Tree, which also has no wildlife section
-      #and is constructed differently
       self.wildlife = "There is no information available on wildlife in this park."
     end
 
-    #Nearby Parks--------------------------------------------------------------
 
     nearby_parks_page = Scraper.get_park_page(self.link).css(".three-col")
 
@@ -147,9 +129,7 @@ class ParkScraper
     self.nearby_parks << park_2
     self.nearby_parks << park_3
 
-    #END OF ATTRIBUTES---------------------------------------------------------
 
-    #fixes strange bug with the construction of Yosemite's information
     if self.name == "Yosemite National Park"
       self.information = "#{self.information.split("<")[0]}"
     end
